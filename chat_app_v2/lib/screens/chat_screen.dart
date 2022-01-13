1b43ba5a051b5183f8ef96c970c73ed54340d306
Shai Herman <shai.herman@au10tix.com>
2022-01-13 16:28:18 +0200

    completed #321

--- chat_app_v2/lib/screens/chat_screen.dart
+++ chat_app_v2/lib/screens/chat_screen.dart
@@ -5,29 +5,34 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('chats/z0mH7hKUSvn83jMZv85Q/messages')
-              .snapshots(),
-          builder: (ctx, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-            if (streamSnapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final documents = streamSnapshot.data!.docs;
-            return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (ctx, i) => Container(
-                padding: const EdgeInsets.all(8),
-                child: Text(documents[i]['text']),
-              ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/z0mH7hKUSvn83jMZv85Q/messages')
+            .snapshots(),
+        builder: (ctx, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
-        ));
+          }
+          final documents = streamSnapshot.data!.docs;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, i) => Container(
+              padding: const EdgeInsets.all(8),
+              child: Text(documents[i]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/z0mH7hKUSvn83jMZv85Q/messages')
+              .add({'text': 'This was added by clicking the button'});
+        },
+      ),
+    );
   }
 }
