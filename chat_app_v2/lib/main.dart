ca25ceeb2e860f4ee8f8f8743a41b0fd1d1dc529
Shai Herman <shai.herman@au10tix.com>
2022-01-13 11:56:52 +0200

    init

---
+++ chat_app_v2/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './screens/chat_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
---
+++ chat_app_v2/lib/screens/chat_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, i) => Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('This works!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/z0mH7hKUSvn83jMZv85Q/messages')
+              .snapshots()
+              .listen((data) {
+            print(data.docs[0]['text']);
+          });
+        },
+      ),
+    );
+  }
+}
